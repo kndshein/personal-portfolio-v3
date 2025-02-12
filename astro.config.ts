35ad11916d85ab11bc41317eb582a3579773cb5e
kndshein <kndshein@gmail.com>
2025-02-11 20:35:45 -0800

    Create OutboundLink component

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import icon from 'astro-icon';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
 });
