07e3d5f46ab988ef375c558819af35e0d872bc2d
kndshein <kndshein@gmail.com>
2025-02-12 01:32:51 -0800

    Fix theme flash + use icon locally

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Icon } from '@iconify/react';
 import '../styles/themetoggler.css';
 
-// const themes: string[] = ['light', 'dark', 'blue', 'green'];
 const themes = [
-  { name: 'light', icon: 'fa6-solid:moon' },
-  { name: 'dark', icon: 'mdi:white-balance-sunny' },
+  {
+    name: 'light',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22C6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: 'dark',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <g fill="none">
+          <g fill="currentColor" clipPath="url(#siSunFill0)">
+            <path d="M12 0a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0V1a1 1 0 0 1 1-1M0 12a1 1 0 0 1 1-1h2a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1m21-1a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2zm-8 10a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0zm-6.657-3.343a1 1 0 0 1 0 1.414L4.93 20.485a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0M20.485 3.515a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1-1.414-1.414l1.414-1.414a1 1 0 0 1 1.414 0m-16.97 0a1 1 0 0 1 1.414 0l1.414 1.414A1 1 0 1 1 4.93 6.343L3.515 4.93a1 1 0 0 1 0-1.414m14.142 14.141a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 1 1-1.414 1.414l-1.414-1.414a1 1 0 0 1 0-1.414M5 12a7 7 0 1 1 14 0a7 7 0 0 1-14 0" />
+          </g>
+          <defs>
+            <clipPath id="siSunFill0">
+              <path fill="#fff" d="M0 0h24v24H0z" />
+            </clipPath>
+          </defs>
+        </g>
+      </svg>
+    ),
+  },
 ];
 
 const ThemeToggler: React.FC = () => {
@@ -37,7 +61,7 @@ const ThemeToggler: React.FC = () => {
         return (
           theme.name == themes[currentThemeIndex].name && (
             <button onClick={toggleTheme} key={theme.name} id="theme-toggler">
-              <Icon icon={theme.icon} />
+              {theme.icon}
             </button>
           )
         );
