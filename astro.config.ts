61b77c5945e8ed0ad6c2296645d4388d63a4fd4a
kndshein <kndshein@gmail.com>
2025-02-11 18:35:34 -0800

    Import React and setup Navbar

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()],
+});
